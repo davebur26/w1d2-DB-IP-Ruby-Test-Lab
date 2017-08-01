5180283da1a1b5b3c12fe115ddf529a4058ae998
David Burnett <davidburnett@Davids-MacBook-Pro.local>
2017-08-01 17:23:34 +0100

    add test_number_to_full_name function

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -29,3 +29,16 @@ end
 def add_string_as_number(num1,num2)
   return num1.to_i + num2.to_i
 end
+
+def number_to_full_month_name( name )
+  case (name)
+  when 1
+    return "January"
+  when 3
+    return "March"
+  when 9
+    return "September"
+  else
+    return "Fail"
+  end
+end
