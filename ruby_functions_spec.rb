70cd6a3679071b2ea550210e54a30fee5e0f55a2
David Burnett <davidburnett@davids-mbp.home>
2017-08-01 21:02:04 +0100

    add updates to test sheet which checks the volume/sphere/temperature definitions

--- ruby_functions_spec.rb
+++ ruby_functions_spec.rb
@@ -3,34 +3,41 @@ require_relative( 'ruby_functions_practice' )
 
 class Functions_Practice < MiniTest::Test
 
+#complete
   def test_return_10()
-    assert_equal( 9, return_10() )
+    assert_equal( 10, return_10() )
   end
 
+#compelte
   def test_add()
     sum = add( 1, 2 )
     assert_equal( 3, sum )
   end
 
+#complete
   def test_subtract()
     difference = subtract( 10, 5 )
     assert_equal( 5, difference )
   end
 
+#complete
   def test_multiply()
     assert_equal( 8, multiply( 4, 2 ) )
   end
 
+#complete
   def test_divide()
     assert_equal( 5, divide( 10, 2 ) )
   end
 
+#complete
   def test_length_of_string()
     test_string = "A string of length 21"
     length = length_of_string( test_string )
     assert_equal( 21, length )
   end
 
+#complete
   def test_join_string()
     string_1 = "Mary had a little lamb, "
     string_2 = "it's fleece was white as snow"
@@ -38,12 +45,14 @@ class Functions_Practice < MiniTest::Test
     assert_equal( "Mary had a little lamb, it's fleece was white as snow", joined_string )
   end
 
+#complete
   def test_add_string_as_number()
     sum_of_strings = add_string_as_number( "1", "2" )
     assert_equal( 3, sum_of_strings )
   end
 
   def test_number_to_full_name()
+
     first_month_string = number_to_full_month_name( 1 )
     third_month_string = number_to_full_month_name( 3 )
     ninth_month_string = number_to_full_month_name( 9 )
@@ -53,6 +62,7 @@ class Functions_Practice < MiniTest::Test
   end
 
   def test_substring()
+
     first_month_string = number_to_short_month_name( 1 )
     third_month_string = number_to_short_month_name( 3 )
     ninth_month_string = number_to_short_month_name( 9 )
@@ -67,17 +77,21 @@ class Functions_Practice < MiniTest::Test
 
   #Given the length of a side of a cube calculate the volume
   def test_volume_of_cube()
-    #add test code here
+    volume = volume_of_cube(3)
+    assert_equal(27,volume)
   end
 
   #Given the radius of a sphere calculate the volume
   def test_volume_of_sphere()
-    #add test code here
+    volume = volume_of_sphere(5)
+    correct_answer = (4/3)*(Math::PI)*(5**3)
+    assert_equal(correct_answer,volume)
   end
 
   #Given a value in farenheit, convert this into celsius.
   def test_fahrenheit_to_celsius()
-    #add test code here
+    temp_c = fahrenheit_to_celsius(0).round(1)
+    assert_equal(-17.8,temp_c)
   end
 
 
